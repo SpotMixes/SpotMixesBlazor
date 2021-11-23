2d79a068bb75ce84019290c417dde167231a955d
Luis Miguel Uriarte Llaja <hello.kiulcode@gmail.com>
2021-11-23 18:00:07 -0500

    ✨ Implementación de la pagina de comentarios y nuevo card para la lista de audios

---
+++ SpotMixesBlazor/Server/Controllers/StorageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Firebase.Storage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpotMixesBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StorageController : Controller
+    {
+        [HttpPost("{folderName}")]
+        public async Task<IActionResult> Upload(string folderName)
+        {
+            try
+            {
+                var formCollection = await Request.ReadFormAsync();
+                var file = formCollection.Files[0];
+
+                if (file.Length <= 0) 
+                    return BadRequest("No se ha podido encontrar el archivo");
+                
+                var responseFirebase = new FirebaseStorage("spotmixes-test.appspot.com",
+                        new FirebaseStorageOptions
+                        {
+                            ThrowOnCancel = true
+                        })
+                    .Child(folderName)
+                    .Child(file.FileName)
+                    .PutAsync(file.OpenReadStream());
+
+                var fileLink = await responseFirebase;
+                return Ok(fileLink);
+            }
+            catch (Exception ex) { 
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
